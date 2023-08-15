d95414ec5f97dfe9154048bb36a6135fbe52857f
jinrong.wu <jinrongw@foxmail.com>
2023-08-15 23:26:28 +0800

    完善用户注册函数

--- src/server/char_service.cc
+++ src/server/char_service.cc
@@ -1,7 +1,9 @@
 #include "../../include/server/char_service.hpp"
 #include "../../include/public.hpp"
+#include "../../include/server/user_model.hpp"
 #include <functional>
 #include <muduo/base/Logging.h>
+#include <jsoncpp/json/json.h>
 
 using namespace std;
 using namespace placeholders;
@@ -51,6 +53,40 @@ void ChatService::login(const TcpConnectionPtr &conn, Json::Value &message_value
 void ChatService::regis(const TcpConnectionPtr &conn, Json::Value &message_value, Timestamp time)
 {
     LOG_INFO << "do regis service.";
+    std::string name = message_value["name"].asString();
+    std::string passwd = message_value["passwd"].asString();
+    LOG_INFO << "name:" << name << " "
+             << "passwd:" << passwd;
+    Json::Value result_value; // 传给客户端的json value
+    Json::FastWriter writer;
+    if (name == "" || passwd == "")
+    {
+        result_value["errno"] = 1;
+        std::string out_json = writer.write(result_value);
+        conn->send(out_json);
+        return;
+    }
+
+    UserModel user_model;
+    User user;
+    user.setName(name);
+    user.setPasswd(passwd);
+    bool status = user_model.insert(user);
+
+    result_value["msg_id"] = REGIS_ACK;
+    if (status)
+    {
+        result_value["errno"] = 0;
+        result_value["id"] = user.getID();
+    }
+    else
+    {
+        result_value["errno"] = 1;
+    }
+
+    std::string out_json = writer.write(result_value);
+
+    conn->send(out_json);
 }
 
 ChatService *ChatService::ins_ = nullptr;
--- src/server/user_model.cc
+++ src/server/user_model.cc
@@ -1,22 +1,25 @@
 #include "../../include/server/user_model.hpp"
 #include "../../../include/server/db/db.hpp"
 #include <string>
+#include <iostream>
 
 bool UserModel::insert(User &user)
 {
     std::string sql;
-    sql += "insert into User(name, password, state) values (";
-    sql += user.getName().c_str() + ',';
-    sql += user.getPasswd().c_str() + ',';
-    sql += user.getState().c_str() + ')';
+    sql += "insert into user(name, password, state) values (";
+    sql += "\"" + user.getName() + "\", \"";
+    sql += user.getPasswd() + "\", \"";
+    sql += user.getState() + "\")";
 
     MySQL mysql;
     if (mysql.connect())
     {
-        mysql.update(sql);
-        user.setID(mysql_insert_id(mysql.getConn()));
-
-        return true;
+        if (mysql.update(sql))
+        {
+            user.setID(mysql_insert_id(mysql.getConn()));
+            return true;
+        }
+        return false;
     }
 
     return false;
