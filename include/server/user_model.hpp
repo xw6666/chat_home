cf64fc605253f61d63acd9bdeb7e44d568be4a85
jinrong.wu <jinrongw@foxmail.com>
2023-08-17 00:07:18 +0800

    实现了服务器端用户登录功能

--- include/server/user_model.hpp
+++ include/server/user_model.hpp
@@ -5,5 +5,10 @@
 class UserModel
 {
 public:
+    // 往数据库表插入一个用户，不传const，因为要顺便填入id
     bool insert(User &user);
+    // 通过id像User表查询用户，将对应字段填入传出型参数user
+    void query(int id, User *user);
+    // 将user中的state更新到user数据库表中
+    bool updateState(const User &user);
 };
